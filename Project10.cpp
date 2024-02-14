e417ef38d24adb8c831e76b923d64be65bcc8867
MISHA <vol.mykhailo@gmail.com>
2024-02-14 22:38:50 +0200

    Добавьте файлы проекта.

---
+++ Project10.cpp
@@ -0,0 +1,103 @@
+﻿#include "Windows.h"
+#include "windowsx.h"
+#include "tchar.h"
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nShowCmd)
+{
+	WCHAR className[] = L"FirstWindow";
+
+	WNDCLASSEX wndClass;
+	wndClass.cbSize = sizeof(WNDCLASSEX);
+	wndClass.style = CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS;
+	wndClass.hInstance = hInstance;
+	wndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndClass.hIconSm = NULL;
+	wndClass.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wndClass.cbClsExtra = 0;
+	wndClass.cbWndExtra = 0;
+	wndClass.lpszMenuName = NULL;
+	wndClass.lpszClassName = className;
+
+	wndClass.lpfnWndProc = WindowProc;
+
+	if (!RegisterClassEx(&wndClass)) {
+		return 0;
+	}
+
+	HWND hWnd = CreateWindowEx(
+		0,
+		className,
+		L"My first window",
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, //x,y
+		CW_USEDEFAULT, CW_USEDEFAULT, //width, height
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);
+
+	ShowWindow(hWnd, nShowCmd);
+	UpdateWindow(hWnd);
+
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0)) {
+		DispatchMessage(&msg);
+	}
+
+	return msg.wParam;
+}
+
+int left_count = 0;
+int right_count = 0;
+int midd_count = 0;
+
+
+
+void KeyDownHandler(HWND hwnd, WPARAM wParam, LPARAM lParam)
+{
+	TCHAR str[100];
+	wsprintf(str, L"left: %4d, right: %4d, midd: %4d", left_count, right_count, midd_count);
+
+	SetWindowText(hwnd, str);
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+
+	switch (uMsg)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0); //WM_QUIT
+		break;
+
+	case WM_LBUTTONUP:
+	{
+		left_count++;
+		KeyDownHandler(hwnd, wParam, lParam);
+		break;
+	}
+
+	case WM_RBUTTONUP:
+	{
+		right_count++;
+		KeyDownHandler(hwnd, wParam, lParam);
+		break;
+	}
+
+	case WM_MBUTTONUP:
+	{
+		midd_count++;
+		KeyDownHandler(hwnd, wParam, lParam);
+		break;
+	}
+
+	default:
+		return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	}
+
+	return 0;
+}
